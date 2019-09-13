419ae86d32842ca1833072c1acd3336355e40f33
Mohamed <mohamedadham27@gmail.com>
2019-09-13 07:14:40 +0200

    Updates

---
+++ precache-manifest.e25612a44f7049983e632995827785fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98375dd4001449cb975d135ee3546349",
+    "url": "/recipe-box/index.html"
+  },
+  {
+    "revision": "27db2f7393fa53a99463",
+    "url": "/recipe-box/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c8334a4a057be51fb008",
+    "url": "/recipe-box/static/css/main.e5634a1a.chunk.css"
+  },
+  {
+    "revision": "27db2f7393fa53a99463",
+    "url": "/recipe-box/static/js/2.8f165109.chunk.js"
+  },
+  {
+    "revision": "c8334a4a057be51fb008",
+    "url": "/recipe-box/static/js/main.a3f314e3.chunk.js"
+  },
+  {
+    "revision": "c45b776f11a294949d39",
+    "url": "/recipe-box/static/js/runtime~main.26152b22.js"
+  }
+]);
